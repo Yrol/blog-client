b2831e8dd90e5342b5b2bcc02ab87f13b9306b97
Yrol <yrollgayanth@yahoo.com>
2020-09-07 23:14:21 +1000

    Pagination component

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,11 @@
         <CategoriesCard />
       </div>
     </div>
+    <div class="flex flex-wrap border-t border-gray-200">
+      <div class="w-full md:w-3/4">
+        <Pagination />
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
@@ -21,12 +26,14 @@ import Navigation from "~/components/site/header.vue";
 import Card from "~/components/site/card.vue";
 import CategoriesCard from "~/components/site/categoriescard.vue";
 import Footer from "~/components/site/footer.vue";
+import Pagination from "~/components/site/pagination.vue";
 export default {
   components: {
     Navigation,
     Card,
     CategoriesCard,
-    Footer
+    Footer,
+    Pagination
   },
   head: {
     title: "Home"
