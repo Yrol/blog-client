e726a6f18fd773aeede380537f7d90c67cda6ec5
Yrol <yrollgayanth@yahoo.com>
2020-09-07 21:56:11 +1000

    Modifications to the categories pane

--- components/site/categoriescard.vue
+++ components/site/categoriescard.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="w-full lg:flex shadow-lg mb-2">
-    <span
-      class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 uppercase last:mr-0 mr-1"
-    >PHP</span>
+    <div
+      class="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+    >
+      <div>
+        <div class="text-black font-bold text-base mb-2">Categories</div>
+        <span
+          class="text-xs mb-2 font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 uppercase last:mr-0 mr-1"
+        >C#</span>
+        <span
+          class="text-xs mb-2 font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 uppercase last:mr-0 mr-1"
+        >PHP</span>
+        <span
+          class="text-xs mb-2 font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 uppercase last:mr-0 mr-1"
+        >PHP</span>
+        <span
+          class="text-xs mb-2 font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 uppercase last:mr-0 mr-1"
+        >PHP</span>
+        <span
+          class="text-xs mb-2 font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 uppercase last:mr-0 mr-1"
+        >PHP</span>
+        <span
+          class="text-xs mb-2 font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 uppercase last:mr-0 mr-1"
+        >PHP</span>
+        <span
+          class="text-xs mb-2 font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 uppercase last:mr-0 mr-1"
+        >PHP</span>
+        <span
+          class="text-xs mb-2 font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 uppercase last:mr-0 mr-1"
+        >PHP</span>
+      </div>
+    </div>
   </div>
 </template>
