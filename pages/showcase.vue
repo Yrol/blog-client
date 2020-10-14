290aee61e3941a1485589759e34a892848ce1781
Yrol <yrollgayanth@yahoo.com>
2020-10-14 21:50:04 +1100

    Addin the showcase page

---
+++ pages/showcase.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navigation />
+    <div class="flex flex-wrap">
+      <div class="w-full md:w-3/4 p-4"></div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Navigation from "~/components/Site/Header";
+export default {
+  components: { Navigation },
+};
+</script>
