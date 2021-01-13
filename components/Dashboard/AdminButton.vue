46748b21522e58493e3c52e8144b27b6516ae0d6
Yrol <yrollgayanth@yahoo.com>
2021-01-13 22:12:48 +1100

    Adding the Dashboard components

---
+++ components/Dashboard/AdminButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <nuxt-link
+    class="admin-button button"
+    type="button"
+    :to="url"
+    :alt="alt"
+    :class="classes"
+  >
+    <slot></slot>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'AdminButton',
+  props: {
+    label: String,
+    rounded: Boolean,
+    round: Boolean,
+    flat: Boolean,
+    url: String,
+    alt: String,
+  },
+  data() {
+    return {
+      buttonClass: '',
+    };
+  },
+  computed: {
+    classes() {
+      return (
+        (this.round === true ? 'rounded-full p-0' : '') +
+        (this.rounded === true ? ' rounded' : '')
+      );
+    },
+  },
+};
+</script>
+
+<style>
+.admin-button {
+  transition: all 0.25s ease;
+}
+</style>
