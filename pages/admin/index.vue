7d48021e261b7ea585db22749553039df7f66bed
Yrol <yrollgayanth@yahoo.com>
2021-01-13 20:10:59 +1100

    Adding the Unauthenticated middleware

---
+++ pages/admin/index.vue
@@ -0,0 +1,22 @@
+<template></template>
+<script>
+export default {
+  name: 'Admin',
+  head: {
+    title: 'Admin',
+  },
+  //middleware: 'unauthinticated',
+  components: {},
+  data() {
+    return {
+      mainOptions: [
+        { slug: 'all-posts', name: 'All Posts' },
+        { slug: 'new-post', name: 'New Post' },
+        { slug: 'new-category', name: 'New Category' },
+        { slug: 'settings', name: 'Settings' },
+        { slug: 'help', name: 'Help' },
+      ],
+    };
+  },
+};
+</script>
