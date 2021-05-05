f36001f0b2611c836d79536a2360ef6884498185
Yrol <yrollgayanth@yahoo.com>
2021-05-05 22:22:58 +1000

    Adding changes to the navigation

--- components/site/PostList.vue
+++ components/site/PostList.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
-    <div class="flex flex-wrap">
-      <div class="w-full md:w-3/4 p-4">
-        <Card
-          v-for="(post, index) in posts"
-          :key="index"
-          :dataReady="true"
-          :toLocation="toLocation"
-          :postData="post"
-        />
-      </div>
-      <div class="w-full md:w-1/4 lg:pl-0 md:pl-0 p-4">
-        <CategoriesCard />
-      </div>
+  <div class="flex flex-wrap">
+    <div class="w-full md:w-3/4 p-4">
+      <Card
+        v-for="(post, index) in posts"
+        :key="index"
+        :dataReady="true"
+        :toLocation="toLocation"
+        :postData="post"
+      />
+    </div>
+    <div class="w-full md:w-1/4 lg:pl-0 md:pl-0 p-4">
+      <CategoriesCard />
     </div>
   </div>
 </template>
